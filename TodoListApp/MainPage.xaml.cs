446054a2b834693ea22cb3bcc7d915ca4829b76a
Kris-fynn <67267786+Kris-fynn@users.noreply.github.com>
2022-12-04 18:26:48 +0200

    Commit by Kris

---
+++ TodoListApp/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TodoListApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void HandleTexttChanged(object sender, TextChangedEventArgs args)
+        {
+            Console.WriteLine(args.NewTextValue);
+        }
+
+        public void HandleTextChanged(object sender, TextChangedEventArgs args) 
+        { 
+           Console.WriteLine(args.NewTextValue);
+        }
+
+        public void HandleEnterPress(object sender, EventArgs args)
+        {
+            Console.WriteLine("Enter Pressed");
+            Label newTodo = new Label();
+            newTodo.Text = InputField.Text;
+            newTodo.FontSize = 20;
+            TodoList.Children.Add(newTodo);
+            InputField.Text = "";
+        }
+           
+    }
+}
---
+++ TodoListApp/TodoListViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+
+namespace TodoListApp
+{
+     public class TodoListViewModel
+    {
+        public ObservableCollection<TodoItem> TodoItems { get; set; }
+        public TodoListViewModel()
+        {
+            TodoItems = new ObservableCollection<TodoItem>();
+
+            TodoItem.Add(new TodoItem("todo 1", false));
+            TodoItem.Add(new TodoItem("todo 2", false));
+            TodoItem.Add(new TodoItem("todo 3", false));
+
+        }
+    }
+}
